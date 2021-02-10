9b25a3731568998f069b05a1f3abc60086230787
Thiago Penna <thiago.penna@icloud.com>
2021-02-10 14:15:47 -0300

    Move RealmCache to its own file

---
+++ FeedStoreChallenge/RealmCache.swift
@@ -0,0 +1,31 @@
+//
+//  RealmCache.swift
+//  FeedStoreChallenge
+//
+//  Created by Thiago Penna on 10/02/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+internal class RealmCache: Object {
+	@objc dynamic var _id: String!
+	@objc dynamic var timestamp: Date = Date()
+	let feed: List<RealmFeedImage> = List<RealmFeedImage>()
+	
+	internal convenience init(_id: String, feed: [RealmFeedImage], timestamp: Date) {
+		self.init()
+		self._id = _id
+		self.feed.append(objectsIn: feed)
+		self.timestamp = timestamp
+	}
+	
+	override class func primaryKey() -> String? {
+		return "_id"
+	}
+	
+	internal var local: [LocalFeedImage] {
+		return feed.map { $0.local }
+	}
+}
--- FeedStoreChallenge/RealmFeedStore.swift
+++ FeedStoreChallenge/RealmFeedStore.swift
@@ -9,27 +9,6 @@
 import Foundation
 import RealmSwift
 
-internal class RealmCache: Object {
-	@objc dynamic var _id: String!
-	@objc dynamic var timestamp: Date = Date()
-	let feed: List<RealmFeedImage> = List<RealmFeedImage>()
-	
-	internal convenience init(_id: String, feed: [RealmFeedImage], timestamp: Date) {
-		self.init()
-		self._id = _id
-		self.feed.append(objectsIn: feed)
-		self.timestamp = timestamp
-	}
-	
-	override class func primaryKey() -> String? {
-		return "_id"
-	}
-	
-	internal var local: [LocalFeedImage] {
-		return feed.map { $0.local }
-	}
-}
-
 internal class RealmFeedImage: Object {
 	@objc dynamic var _id: String!
 	@objc dynamic var desc: String?
